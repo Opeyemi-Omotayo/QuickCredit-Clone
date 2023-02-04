4a46af3a8af790dbfa804250ba6538dca549cabc
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-02-04 21:24:50 +0100

    about compoent

--- src/components/header/NavLinks.js
+++ src/components/header/NavLinks.js
@@ -6,14 +6,14 @@ import './NavLinks.css';
 const NavLinks = () => {
     return <React.Fragment>
           <ul className="fl_right navStyle">
-          {/* <li><NavLink to="/">ABOUT</NavLink></li>
+           <li><NavLink to="/">ABOUT</NavLink></li>
             <li>
             <NavLink to="/process">THE PROCESS</NavLink>
             </li>
             <li><NavLink to="/whyus">WHY US</NavLink></li>
             <li><NavLink to="/faqs">FAQS</NavLink></li>
             <li><NavLink to="/login">LOGIN</NavLink></li>
-            <li><NavLink to="/register">REGISTER</NavLink></li> */}
+            <li><NavLink to="/register">REGISTER</NavLink></li>
           </ul>
     </React.Fragment>
 };
